50b83a8a32becedf9de5a43cb819e4f6712cc194
Emilien Bondu <emilien.bondu@ollca.com>
2025-12-10 09:39:40 +0100

    update to ng19

--- projects/angular6-example-app/src/app/app.component.ts
+++ projects/angular6-example-app/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { KeycloakService } from '@ebondu/angular-keycloak';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   title = 'angular6-example-app';
